9caa2bc83dc22426e65fb5e3b5ecaaed8ec257f5
dineshcodx <codexjavadsa@gmail.com>
2026-03-15 20:21:48 +0530

    feat: add CRUD APIS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
-const app = express(); // create an express app
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+
+const app = express();
 
 app.use(express.json());
 
-import userRouter from "./routes/user.route.js";
-
 app.use("/api/v1/users", userRouter);
-//example route http://localhost:4000/api/v1/users/register
+app.use("/api/v1/posts", postRouter);
+
 export default app;
